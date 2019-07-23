33953ee34efa16bd868aafb60e654f37949d24d8
howinator <howiebenefiel@gmail.com>
2019-07-22 20:45:07 -0500

    Make the invitation layout work

--- app/inforsvp/static/inforsvp/inforsvp.js
+++ app/inforsvp/static/inforsvp/inforsvp.js
@@ -1,6 +1,9 @@
 // todo only do this in dev
 window.setTimeout(() => {
-    location.href = "http://localhost:7665";
+    const reload = false;
+    if (reload){
+        location.href = "http://localhost:7665";
+    }
 }, 5000);
 
 $(() => {
