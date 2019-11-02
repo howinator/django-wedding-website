343e38f5d9dcfb8d0e4026f5851d9dc67d252d0b
howinator <howiebenefiel@gmail.com>
2019-11-02 18:40:48 -0500

    rm rsvp

--- app/inforsvp/urls.py
+++ app/inforsvp/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path(r'rsvp', views.rsvp, name='rsvp'),
-    path(r'rsvp/', views.rsvp, name='rsvp'),
+    # path(r'rsvp', views.rsvp, name='rsvp'),
+    # path(r'rsvp/', views.rsvp, name='rsvp'),
     path(r'thanks', views.thanks, name='thanks'),
     path(r'thanks/', views.thanks, name='thanks'),
     path(r'need_help', views.need_help, name='need_help'),
